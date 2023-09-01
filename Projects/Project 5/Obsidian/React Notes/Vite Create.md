4bd49d1bcd403d1ae07864435d15a29b69469ec3
Krishna <kpratapsingh026@gmail.com>
2023-09-02 00:23:44 +0530

    Shoes Website

---
+++ Projects/Project 5/Obsidian/React Notes/Vite Create.md
@@ -0,0 +1,11 @@
+`npm create vite@latest`
+
+`cd project-1
+`npm install`
+`npm run dev` 
+
+
+Move code up `alt + upArrow`
+Copy line `Ctrl + alt + upOrDownArrow`
+Break infinite line `alt + z`
+Align All item `Shit + alt + f` 
---
+++ Projects/Project 5/project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HeroSection from './components/Hero'
+import Navigation from './components/Navigation'
+const App = () =>{
+  return (
+    <>
+    <Navigation></Navigation>
+    <HeroSection></HeroSection>
+    </>
+  )
+}
+
+export default App
